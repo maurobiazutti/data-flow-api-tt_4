be0a38358d9bd436f329eabfd158ed468e16e244
Mauro <mauro_biazutti@hotmail.com>
2026-07-03 22:55:51 -0300

    feat: desenvolvendo action create

--- data-flow-api/app/controllers/imports_controller.rb
+++ data-flow-api/app/controllers/imports_controller.rb
@@ -1,5 +1,12 @@
 class ImportsController < ApplicationController
   def create
-    render json: {message: "Import endpoint reached"}, status: :created
+    uploaded_file = params[:file]
+
+    if uploaded_file
+      json_data = CsvImportService.new(uploaded_file).call
+      render json: json_data, status: :created
+    else
+      render json: {error: "Arquivo ausente"}, status: :bad_request
+    end
   end
 end
